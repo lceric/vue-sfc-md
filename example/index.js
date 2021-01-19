cac6af0d40dbbd7181b02ea55ac7081c4ab60b35
Eric <lichao@kaike.la>
2021-01-19 11:27:30 +0800

    feat: support remove empty columns

--- example/index.js
+++ example/index.js
@@ -1,17 +1,22 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 const { parsed } = require('../lib')
 /**
  * 主线程
  */
 async function main() {
-  const sourceStr = fs.readFileSync(
-    path.join(__dirname, './Btn.vue'),
-    'utf8'
-  )
+  const sourceStr = fs.readFileSync(path.join(__dirname, './Btn.vue'), 'utf8')
 
-  let docModules = await parsed(sourceStr)
-  let sfcDoc = docModules.filter(m => !m.empty).map(m => m.doc).join('\n')
+  let docModules = await parsed(sourceStr, {
+    jsx: true,
+    removeEmptyColumns: true,
+    emptyFlags: ['-', '`false`'],
+  })
+
+  let sfcDoc = docModules
+    .filter((m) => !m.empty)
+    .map((m) => m.doc)
+    .join('\n')
   console.log(sfcDoc)
 }
 
